0e3440280c4b2bda1826d0be3cccf24c3384555b
zalfarmdhn <zalfarmdhn1011@gmail.com>
2024-03-09 01:31:09 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+const lyricText = document.getElementById('lyrics-text'); 
+const audio = document.getElementById('audio');
+
+const lyrics = [
+    { time: "15:06", text: 'Karena kamu aku rela menunggu semua' }, 
+    { time: "21:10", text: 'Sungguh berat yang ku rasa' }, 
+    { time: "23:04", text: 'Karena kamu aku tetap bertahan' }, 
+    { time: "27:03", text: 'Meskipun kini engkau di peluknya' }, 
+    { time: "31:27", text: 'Masih mungkinkah semua abadi seperti dahulu' }, 
+    { time: "38:87", text: 'KARENAMU' }, 
+    { time: "39:60", text: 'SELALU' }, 
+    { time: "40:41", text: 'KARENAMU' }, 
+    { time: "42:04", text: 'KU CEMBURU...' },
+    { time: "46:79", text: 'Teruslah melangkah melupakan dirinya' }, 
+    { time: "50:18", text: 'Tinggalkan dia untukku sekarang' },
+    { time: "54:53", text: 'Saatnya tunjukkan semua tentang hatimu' },
+    { time: "58:10", text: 'Bahwa akulah yang pantas untukmu' },
+    { time: "61:49", text: 'Hanya kamu di setiap helahan nafasku' },
+    { time: "69:50", text: 'Hanya kamu, hanya kamu' },
+    { time: "75:00", text: '--------SELESAI--------' },
+];
+
+function milisecondsChanger(time) {
+    let splitTime = time.split(':');
+    let seconds = parseInt(splitTime[0]);
+    let milliseconds = parseInt(splitTime[1]);
+    return (seconds * 1000) + milliseconds;
+}
+
+function setTiming(lyrics) {
+    lyricText.innerText = 'Enjoy the music!';
+    audio.setAttribute('hidden', true);
+    for (const lyric of lyrics) {
+        setTimeout(function() {
+            lyricText.innerText = `${lyric.text}`;
+        }, milisecondsChanger(lyric.time));
+    }
+}
+
+audio.addEventListener('play', function() {
+    setTiming(lyrics);
+    document.querySelector('#play-pause-icon').className = 'ph-bold ph-pause';
+});
